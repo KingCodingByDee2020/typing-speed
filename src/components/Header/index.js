d27cd0deaad4005dc1e9e49a4547ab6ac6b2df42
Manav Misra <manavm@visionwebsoft.com>
2021-02-28 10:00:48 -0600

    :sparkles: Render styled Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="font-mono pt-4 text-7xl text-center text-neon-green">
+      Typing Speed Test
+    </header>
+  );
+}
+
+export default Header;
