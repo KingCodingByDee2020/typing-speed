3c281fd6e48c2e0be6cb0132c5a29509af12c8e8
Manav Misra <manavm@visionwebsoft.com>
2021-03-03 16:50:45 -0600

    :construction: Log number of secs from Form

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,9 +2,14 @@ import TimerForm from "./TimerForm";
 import TypingArea from "./TypingArea";
 
 function Main() {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event.target.elements[0].value);
+  }
+
   return (
     <main className="flex flex-col gap-4 items-center mx-auto w-96">
-      <TimerForm />
+      <TimerForm handler={handleSubmit} />
       <TypingArea />
     </main>
   );
