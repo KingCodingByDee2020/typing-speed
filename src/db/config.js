0d32f643e356f871dca463f42ac05d0718d60722
Manav Misra <manavm@visionwebsoft.com>
2021-03-04 18:54:45 -0600

    :wrench: Firestore

---
+++ src/db/config.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: "AIzaSyCUM4tdkknwtzOBOFeWXpvow9_2xbRaQHc",
+  authDomain: "typing-scores-59df0.firebaseapp.com",
+  projectId: "typing-scores-59df0",
+  storageBucket: "typing-scores-59df0.appspot.com",
+  messagingSenderId: "1026706668937",
+  appId: "1:1026706668937:web:ac92dc9b3ea38331e4f558",
+};
+
+export default config;
